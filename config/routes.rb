f4fbad8bec34debf2014f7caecbfaa98635f038a
EC2 Default User <ec2-user@ip-172-31-7-3.ec2.internal>
2022-06-26 07:10:04 +0000

    [finish]応用課題4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   # devise_for :users, controllers: {
   #   sessions: 'devise/sessions',
   #   registrations: 'devise/registrations'
@@ -14,9 +16,10 @@ Rails.application.routes.draw do
 	   resource :book_comments, only: [:create, :destroy]
   end
 
-  resources :users, only: [:index,:show,:edit,:update]
-
-
-
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
